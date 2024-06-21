fe84c989c04e827ac4205d2166ac6886c4393a72
konstantin <mailspace.hub@gmail.com>
2024-06-21 18:55:53 +0600

    feat/DTM-174: добавил общий класс исключений AdapterTmsException

--- adaptertms/src/main/java/tech/saas/driver/adaptertms/exception/TmsMessageParsingException.java
+++ adaptertms/src/main/java/tech/saas/driver/adaptertms/exception/TmsMessageParsingException.java
@@ -4,7 +4,7 @@ package tech.saas.driver.adaptertms.exception;
 /**
  * Исключение, возникающее при проблеме в обработке сообщений из TMS.
  */
-public class TmsMessageParsingException  extends RuntimeException {
+public class TmsMessageParsingException  extends AdapterTmsException {
 
     /**
      * Создает новое исключение с указанным сообщением об ошибке.
--- adaptertms/src/main/java/tech/saas/driver/adaptertms/exception/TmsPayloadHandlerNotFoundException.java
+++ adaptertms/src/main/java/tech/saas/driver/adaptertms/exception/TmsPayloadHandlerNotFoundException.java
@@ -4,7 +4,7 @@ package tech.saas.driver.adaptertms.exception;
 /**
  * Исключение, возникающее при поиске обработчика сущности из TMS.
  */
-public class TmsPayloadHandlerNotFoundException extends RuntimeException {
+public class TmsPayloadHandlerNotFoundException extends AdapterTmsException {
 
     public TmsPayloadHandlerNotFoundException(String message) {
         super(message);
